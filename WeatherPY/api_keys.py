e59c8db90f2e6230e701e75a77c7513c877c3826
Fhern1954 <63937814+Fhern1955@users.noreply.github.com>
2021-01-15 16:16:28 -0500

    Finally got api request fixed. Added first 4 graphs

--- WeatherPY/api_keys.py
+++ WeatherPY/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "8ce4e12c6f4d7988eea78b94633a1328"
 
 # Google API Key
-g_key = "YOUR KJEY HERE!"
+g_key = "YOUR KEY HERE!"
